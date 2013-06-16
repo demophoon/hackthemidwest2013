7fd1a705f34b2495e99cf22c116f4aa512d61e88
Britt Gresham <brittcgresham@gmail.com>
2013-06-15 23:14:42 -0500

    Hurray that worked! NOW DON'T TOUCH IT

--- src/leDraw.ino
+++ src/leDraw.ino
@@ -1,15 +1,12 @@
 #include <Adafruit_NeoPixel.h>
-#include <SoftwareSerial.h>
 #include <serLCD.h>
-#include <math.h>
 
 const int TOTAL_DIGITAL_PINS = 13;
-const int WIDTH = 24;
+const int WIDTH = 22;
 const int HEIGHT = 20;
 
-const int internalLED = 13;
-
 Adafruit_NeoPixel strips[(int)WIDTH];
+Adafruit_NeoPixel strip;
 
 void setup() {
     Serial.begin(9600);
@@ -20,46 +17,35 @@ void setup() {
         strips[x].begin();
         strips[x].show();
     }
-    
-    pinMode(internalLED, OUTPUT);
 }
 
+int incrementLoop = 0;
 void loop() {
-  digitalWrite(internalLED, HIGH);
-  delay(100);
-  digitalWrite(internalLED, LOW);
-  delay(400);
+  strips[incrementLoop++].show();
+  if (incrementLoop > WIDTH)
+    incrementLoop = 0;
+  delay(5);
 }
 
 char msg[5];
 int msgPos = 0;
 
 void processMessage(){
-  Serial.println("-");  
   int x = (int)msg[0];
   int y = (int)msg[1];
   int r = (int)msg[2];
   int g = (int)msg[3];
   int b = (int)msg[4];
+  
+  if (x == 11)
+    y--;
 
   strips[x].setPixelColor(y, strips[x].Color(r, g, b));
-
-//  Serial.print("X:");
-//  Serial.println(x);
-//  Serial.print("Y:");
-//  Serial.println(y);
-//  Serial.print("R:");
-//  Serial.println(r);
-//  Serial.print("G:");
-//  Serial.println(g);
-//  Serial.print("B:");
-//  Serial.println(b);
 }
 
 void serialEvent() {
  
     while (Serial.available() > 0) {
-        Serial.print(".");
         msg[msgPos++] = Serial.read();
         if (msgPos == 5){
           msgPos = 0;
@@ -67,27 +53,3 @@ void serialEvent() {
         }
     }
 }
-
-//void serialEvent() {
-//    String incomingString;
-//    while (Serial.available() > 0) {
-//        // read the incoming byte:
-//        delay(2);
-//        char incomingByte = Serial.read();
-//        incomingString += (char)incomingByte;
-//    }
-//    String currentLight = "";
-//    for (int x=0; x < incomingString.length(); x++) {
-//        if (incomingString == ":")
-//            break;
-//        currentLight += incomingString[x];
-//    }
-//    int curLight = currentLight.toInt();
-//    int curX = ceil((float)curLight / (float)WIDTH);
-//    int curY = curLight % HEIGHT;
-//    Serial.println(currentLight);
-//    Serial.println(curX);
-//    Serial.println(curY);
-//    strips[curX].setPixelColor(curY, strips[curX].Color(255, 255, 255));
-//    strips[curX].show();
-//}
